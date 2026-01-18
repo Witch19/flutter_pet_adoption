091123ebb1f115aa0a91912bd1ec0d94ca7d9773
JOSS COMPU <josytst.js@gmail.com>
2026-01-17 22:37:29 -0500

    completo

--- flu_pet_adoption/lib/features/home/home_screen.dart
+++ flu_pet_adoption/lib/features/home/home_screen.dart
@@ -267,18 +267,3 @@ class _RefugioCard extends StatelessWidget {
     );
   }
 }
-import 'package:flutter/material.dart';
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Pet Adoption Home")),
-      body: const Center(
-        child: Text("Bienvenido a Pet Adoption"),
-      ),
-    );
-  }
-}
